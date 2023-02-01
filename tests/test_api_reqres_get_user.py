b835e2eb5d46a548aa791bc501e468f95519ab43
DmanYV <dima-xxx@mail.ru>
2023-02-01 19:10:01 +0300

    - add tests - add schame - add requirements.txt

---
+++ tests/test_api_reqres_get_user.py
@@ -0,0 +1,51 @@
+import requests
+from pytest_voluptuous import S
+from requests import Response
+
+from schemas.user import single_user_schema
+
+
+def test_get_users_status_code():
+    response: Response = requests.get(url='https://reqres.in/api/users/2')
+    print(response.status_code)
+
+    assert response.status_code == 200
+    assert response.json()['data']['email'] == 'janet.weaver@reqres.in'
+
+
+def test_get_users_has_avatar_field():
+    response = requests.get(url='https://reqres.in/api/users/2')
+    print(response.status_code)
+
+    assert response.status_code == 200
+    assert response.json()['data']['avatar']
+    assert response.json().get('data').get('avatar', None)
+
+
+def test_get_users_has_avatar_exist():
+    response: Response = requests.get(url='https://reqres.in/api/users/2')
+    print(response.status_code)
+    avatar = response.json().get('data').get('avatar')
+    results = requests.get(avatar)
+    assert results.status_code == 200
+    assert len(response.content) != 0
+    assert len(response.content) == 280
+
+
+def test_get_users_schema():
+    response = requests.get('https://reqres.in/api/users/2')
+    assert response.status_code == 200
+    assert S(single_user_schema) == response.json()
+
+
+def test_123():
+    response = requests.get('https://reqres.in/api/users?page=2')
+    for first_name in response.json()['data']:
+        print(first_name["first_name"])
+    write_name = first_name["first_name"]
+    file = open("perser.txt", 'w')
+    file.write(write_name)
+    file.close()
+
+
+
